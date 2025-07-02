ff8fb540b5553f98dbf5a51f505ae11a8d2eb614
tejaspokale22 <tejaspokale22@gmail.com>
2025-07-02 15:37:07 +0530

    add suspense

--- app/shop/page.jsx
+++ app/shop/page.jsx
@@ -3,13 +3,16 @@ import EFooter from "@/e-components/Footer";
 import Offer from "@/e-components/Offer";
 import NewsLetter from "@/e-components/NewsLetter";
 import ShopClient from "@/e-components/ShopClient";
+import { Suspense } from "react";
 
 export default function ShopPage() {
   return (
     <div className="min-h-screen bg-white">
       <Offer />
       <EHeader />
-      <ShopClient />
+      <Suspense fallback={<div>Loading shop...</div>}>
+        <ShopClient />
+      </Suspense>
       <NewsLetter />
       <EFooter />
     </div>
