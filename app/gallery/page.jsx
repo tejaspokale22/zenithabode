7aaffdda3f8b8c1bcd88e8336519c7fcc2f35fef
tejaspokale22 <tejaspokale22@gmail.com>
2025-07-22 22:44:59 +0530

    fix

--- app/gallery/page.jsx
+++ app/gallery/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import assets from "@/public/assets";
+import assets from "@/public/assets.js";
 import { useState, useEffect, useRef } from "react";
 
 const galleryImages = [
